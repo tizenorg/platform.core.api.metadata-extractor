596a5838b342f20423aae12beed2d845e4692c04
Minje Ahn <minje.ahn@samsung.com>
2016-01-15 15:13:50 +0900

    Update document
    Change-Id: I744bb5023710be1c9ba02537f2a49e5aa4616753
    Signed-off-by: Minje Ahn <minje.ahn@samsung.com>

---
+++ doc/metadata_extractor_doc.h
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2011 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the License);
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an AS IS BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+#ifndef __TIZEN_MEDIA_METADATA_EXTRACTOR_DOC_H__
+#define __TIZEN_MEDIA_METADATA_EXTRACTOR_DOC_H__
+
+/**
+ * @file metadata_extractor_doc.h
+ * @brief File contains the high level documentation for the Metadata Extractor API.
+ *
+ */
+
+/**
+ * @defgroup CAPI_METADATA_EXTRACTOR_MODULE Metadata Extractor
+ * @ingroup CAPI_MEDIA_FRAMEWORK
+ */
+
+/**
+ * @addtogroup CAPI_METADATA_EXTRACTOR_MODULE
+ * @brief The @ref CAPI_METADATA_EXTRACTOR_MODULE API provides functions for extracting the metadata from an input media file.
+ * @ingroup CAPI_MEDIA_FRAMEWORK
+  * @section CAPI_METADATA_EXTRACTOR_MODULE_HEADER Required Header
+ *    \#include <metadata_extractor.h>
+ *
+ * @section CAPI_METADATA_EXTRACTOR_OVERVIEW Overview
+ * The @ref CAPI_METADATA_EXTRACTOR_MODULE API allows you to extract the metadata from a media file. To extract a particular type of metadata(#metadata_extractor_attr_e), create/destory the handle(#metadata_extractor_create()/#metadata_extractor_destroy). To set the path for extracting, and call metadata_extractor_get_metadata().
+ */
+
+#endif /* __TIZEN_MEDIA_METADATA_EXTRACTOR_DOC_H__ */
+
+
